a7c5c0122c652ddfe2a3836fd5d5dc899c433ac6
olivergraininge <oliver@grainge.me>
2024-04-24 22:50:53 +0100

    cahnging matcher to work with cameras

--- VSLAM/FeatureTrackers/bruteforcematcher.py
+++ VSLAM/FeatureTrackers/bruteforcematcher.py
@@ -52,3 +52,44 @@ class BruteForceTracker(ABCFeatureTracker):
             new_desc_cur,
         )
 
+
+
+class BruteForceTracker(ABCFeatureTracker):
+    def __init__(self, lowe_ratio=0.75):
+        self.lowe_ratio = 0.75
+        self.binary_matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
+        self.dense_matcher = cv2.BFMatcher()
+
+    def track(
+            self,
+            camera1, 
+            camera2
+        ) -> Tuple[List, np.ndarray, List, np.ndarray]:
+
+        if camera1.desc2d.dtype == np.float32:
+            matches = self.dense_matcher.knnMatch(camera2.desc2d, camera1.desc2d, k=2)
+        else: 
+            matches = self.binary_matcher.knnMatch(camera2.desc2d, camera1.desc2df, k=2)
+        # use lowes ratio test
+        good_matches = []
+        for m, n in matches:
+            if m.distance < self.lowe_ratio * n.distance:
+                good_matches.append(m)
+        matches = good_matches
+
+        new_kp_ref = []
+        new_kp_cur = []
+        new_desc_ref = np.zeros((len(matches), camera1.desc2d.shape[1]), dtype=camera1.desc2d.dtype)
+        new_desc_cur = np.zeros((len(matches), camera2.desc2d.shape[1]), dtype=camera2.desc2d.dtype)
+
+        for idx, match in enumerate(matches):
+            new_desc_cur[idx] = camera2.desc2d[match.queryIdx]
+            new_desc_ref[idx] = camera1.desc2d[match.trainIdx]
+            new_kp_cur.append(camera2.kp[match.queryIdx])
+            new_kp_ref.append(camera1.kp[match.trainIdx])
+
+        camera1.kp = new_kp_ref,
+        camera2.kp = new_kp_cur
+        camera1.desc2d = new_desc_ref
+        camera2.desc2d = new_desc_cur
+        return camera1, camera2
