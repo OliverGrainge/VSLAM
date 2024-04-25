5225092aa68d7250f1b493690f82de1aef9897cf
olivergraininge <oliver@grainge.me>
2024-04-25 15:58:12 +0100

    adding tracker top class:

---
+++ VSLAM/FeatureTrackers/tracker.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+from VSLAM.FeatureTrackers.Trackers import KLTTracker, MatcherTracker
+
+from ..utils import get_config
+
+config = get_config()
+
+
+def FeatureTracker():
+    if config["LocalFeatureTracker"] == "KLT":
+        tracker = KLTTracker()
+        tracker.type = "KLT"
+        return tracker
+    elif config["LocalFeatureTracker"] == "Matcher":
+        tracker = MatcherTracker()
+        tracker.type = "Matcher"
+        return tracker
+    else:
+        raise NotImplementedError
