9b5ba675c9ad0ce1dc3a6070e412cdb641daeeee
olivergraininge <oliver@grainge.me>
2024-05-30 11:41:32 +0100

    adjusted bundle adjustment

--- VSLAM/Map/map.py
+++ VSLAM/Map/map.py
@@ -20,105 +20,35 @@ class Map:
 
     def __call__(self, camera: StereoCamera, tracking_info):
         self.cameras.append(camera)
-        query_cam = self.cameras[-1]
         
         if len(self.cameras) >= self.window: 
-            """
-            params, n_cameras = cameras2params(self.cameras, self.window)
-            params2cameras(params, self.cameras, self.window)
-            da = data_assocation(self.cameras, self.window)
-            obs = get_obs_from_cameras(self.cameras, self.window)
-            kl = self.cameras[-1].kl
-            cost_before = np.abs(ba_cost(params, n_cameras, np.zeros(5), kl, obs, da))
-            result = least_squares(
-                ba_cost, 
-                params, 
-                args=(
-                    n_cameras,
-                    np.zeros(5),
-                    kl,
-                    obs, 
-                    da
-                ),
-                #method='lm',
-                ftol=1e-3, 
-                xtol=1e-3, 
-                gtol=1e-3,
-                loss='huber',
-                f_scale=2.5
-            )
-            cost_after = np.abs(ba_cost(result.x, n_cameras, np.zeros(5), kl, obs, da))
-            params2cameras(result.x, self.cameras, self.window)
-            print("cost_before", np.mean(cost_before), "cost_after", np.mean(cost_after))
-
-
-            """
-            """
-
-           
-            pts3d = transform_points3d(query_cam.kpoints3d, query_cam.x)
-            x = np.linalg.inv(query_cam.x)
-            rvec = cv2.Rodrigues(x[:3, :3])[0]
-            tvec = x[:3, 3]
-            obs = query_cam.left_kpoints2d
-            res_self, _ = reprojection_error(pts3d, rvec, tvec, np.zeros(5), query_cam.kl, obs)
-            
-            
-
-            for assoc, cam in zip(da, self.cameras[-self.window:-1]):
-                pts3d = transform_points3d(query_cam.kpoints3d[assoc[0]], query_cam.x)
-                obs = cam.left_kpoints2d[assoc[1]]
-                x = cam.x 
-                x = np.linalg.inv(x)
-                rvec = cv2.Rodrigues(x[:3, :3])[0]
-                tvec = x[:3, 3]
-                res, pp = reprojection_error(pts3d, rvec, tvec, np.zeros(5), cam.kl, obs)
-                print(np.mean(res), np.mean(res_self))
-
-            matches = np.array(
-                [
-                    cv2.DMatch(_queryIdx=assoc[0][idx], _trainIdx=assoc[1][idx], _imgIdx=0, _distance=0)
-                    for idx in range(len(assoc[0]))
-                ]
-            )
-            sample_matches = np.random.randint(0, len(assoc[0]), size=(8,))
-            matches = matches[sample_matches]
-            stereo_matches = cv2.drawMatches(
-                query_cam.left_image,
-                query_cam.left_kp,
-                cam.left_image,
-                cam.left_kp,
-                matches,
-                None,
-                flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
-            )
-            matches = np.array(
-                [
-                    cv2.DMatch(_queryIdx=assoc[0][idx], _trainIdx=idx, _imgIdx=0, _distance=0)
-                    for idx in range(len(assoc[0]))
-                ]
-            )
-            matches = matches[sample_matches]
-            stereo_matches2 = cv2.drawMatches(
-                query_cam.left_image,
-                query_cam.left_kp,
-                cam.left_image,
-                pts2kp(pp),
-                matches,
-                None,
-                flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
-            )
-            print(np.mean(res))
-            print("hi")
-            import matplotlib.pyplot as plt 
-            plt.hist(res, bins=np.linspace(res.min(), res.max(), 100))
-            plt.show()
-            cv2.imshow("Map Matches", stereo_matches)
-            cv2.imshow("Map Reprojections", stereo_matches2)
-            cv2.waitKey(0)
-            """
-
-
+            if config["LocalOptimization"] == "BundleAdjustment":
+                params, n_cameras = cameras2params(self.cameras, self.window)
+                params2cameras(params, self.cameras, self.window)
+                da = data_assocation(self.cameras, self.window)
+                obs = get_obs_from_cameras(self.cameras, self.window)
+                kl = self.cameras[-1].kl
+                cost_before = np.abs(ba_cost(params, n_cameras, np.zeros(5), kl, obs, da))
+                result = least_squares(
+                    ba_cost, 
+                    params, 
+                    args=(
+                        n_cameras,
+                        np.zeros(5),
+                        kl,
+                        obs, 
+                        da
+                    ),
+                    #method='lm',
+                    ftol=1e-3, 
+                    xtol=1e-3, 
+                    gtol=1e-3,
+                    loss='huber',
+                    f_scale=2.5
+                )
+                cost_after = np.abs(ba_cost(result.x, n_cameras, np.zeros(5), kl, obs, da))
+                params2cameras(result.x, self.cameras, self.window)
+                print("cost_before", np.mean(cost_before), "cost_after", np.mean(cost_after))
 
         self.count += 1
 
